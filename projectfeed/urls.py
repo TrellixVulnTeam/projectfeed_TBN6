cd1be49fa294bd8c209ad32eeb725b3b541b5ff1
ludwig <ludwig.brandt@erdwaermeboher.de>
2018-03-08 20:26:44 +0100

    Initial S3 commit

--- projectfeed/urls.py
+++ projectfeed/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, url
+from django.contrib.auth import views as auth_views
+
 from core import views
 
 urlpatterns = [
