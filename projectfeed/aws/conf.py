649dadbcc2d5cdb2c9ea51df48d27e0fb1b3f53b
ludwig <ludwig.brandt@erdwaermeboher.de>
2018-03-08 20:07:18 +0100

    Initial S3 commit

--- projectfeed/aws/conf.py
+++ projectfeed/aws/conf.py
@@ -1,25 +1,16 @@
-import datetime
-AWS_ACCESS_KEY_ID = "AKIAJZIBZZEOW2ET4FNA"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_FILE_EXPIRE = 200
-AWS_PRELOAD_METADATA = True
-AWS_QUERYSTRING_AUTH = True
+import os
 
-DEFAULT_FILE_STORAGE = 'projectfeed.aws.utils.MediaRootS3BotoStorage'
-STATICFILES_STORAGE = 'projectfeed.aws.utils.StaticRootS3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'projectfeed'
-S3DIRECT_REGION = 'us-west-2'
-S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = MEDIA_URL
-STATIC_URL = S3_URL + 'static/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-two_months = datetime.timedelta(days=61)
-date_two_months_later = datetime.date.today() + two_months
-expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
-
-AWS_HEADERS = { 
-    'Expires': expires,
-    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+AWS_ACCESS_KEY_ID = 'AKIAIDWPMVTTLNVE5W6A'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'projectfeed3'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
 }
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'projectfeed/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- projectfeed/settings/production.py
+++ projectfeed/settings/production.py
@@ -135,4 +135,17 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
-from projectfeed.aws.conf import *
+AWS_ACCESS_KEY_ID = 'AKIAIDWPMVTTLNVE5W6A'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'projectfeed3'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'projectfeed/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
