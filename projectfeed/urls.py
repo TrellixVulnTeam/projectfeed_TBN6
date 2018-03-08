6a4e2f0f46f22006a108dd9a1c304a697df22457
ludwig <ludwig.brandt@erdwaermeboher.de>
2018-03-08 20:31:59 +0100

    Initial S3 commit

--- projectfeed/urls.py
+++ projectfeed/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import *
+from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 from core import views
