5a6ccf378c8f8891a99be86cd9e2b868da7eb6fd
ludwig <ludwig.brandt@erdwaermeboher.de>
2018-03-08 20:23:21 +0100

    Initial S3 commit

--- projectfeed/settings/production.py
+++ projectfeed/settings/production.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'projectfeed.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'projectfeed/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
